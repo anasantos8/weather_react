b819d04230dfef213277ae14cd737f41d75155ba
anasantos8 <anarsantos8@gmail.com>
2020-03-22 15:15:27 +0000

    add weath react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello world....</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
